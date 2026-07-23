a450e6ad7b873385efc3d8e7e88c9feaeb4e3b45
Princeton Afeez <princetonafeez@gmail.com>
2026-07-23 16:24:58 -0700

    Django AppConfig for conversions

---
+++ apps/conversions/apps.py
@@ -0,0 +1,7 @@
+# "Django AppConfig for conversions."
+from django.apps import AppConfig
+
+
+class ConversionsConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "apps.conversions"
