c608f42feb4d09107c475cbbe2f67a83c9f7e5a4
Princeton Afeez <princetonafeez@gmail.com>
2026-07-23 17:06:05 -0700

    Django AppConfig for organizations

---
+++ apps/organizations/apps.py
@@ -0,0 +1,7 @@
+# "Django AppConfig for organizations."
+from django.apps import AppConfig
+
+
+class OrganizationsConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "apps.organizations"
