94e100f1a70ce48b40f28ac7c83a069ce7ee2876
Princeton Afeez <princetonafeez@gmail.com>
2026-07-23 17:13:05 -0700

    Django AppConfig for quotas

---
+++ apps/quotas/apps.py
@@ -0,0 +1,7 @@
+# "Django AppConfig for quotas."
+from django.apps import AppConfig
+
+
+class QuotasConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "apps.quotas"
